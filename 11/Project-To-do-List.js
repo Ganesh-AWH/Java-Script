1edb9609ae98500f4f31b87fcb760e2ed54529f3
Ganesh <ganesh02003@gmail.com>
2023-08-06 21:15:22 +0530

    modified

---
+++ 11/Project-To-do-List.js
@@ -0,0 +1,47 @@
+let todoList = [{
+  name: '',
+  dueDate: ''
+}];
+function todoAdd(){
+  
+  const inputElement = document.querySelector('.js-name-input');
+  const dateElement = document.querySelector('.js-date-input');
+
+  const name = inputElement.value;
+  const dueDate = dateElement.value; 
+  
+  if(name && dueDate){
+    todoList.push({
+      name,
+      dueDate
+      //short Hand
+      // name: name,
+      // dueDate: dueDate,
+  });
+  }
+
+  inputElement.value = '';
+  displayList();
+}
+function displayList(){
+  let toListHtml = '';
+  for(let i in todoList){
+
+    const toDoObject = todoList[i];
+    const {name,dueDate} = toDoObject;
+    // const name = toDoObject.name;
+    // const duDate = toDoObject.dueDate;
+    if(name && dueDate){
+      toListHtml += `
+        <div class="todo-output">${name}</div>
+        <div class="todo-output">${dueDate}</div>
+        <button onclick="
+          todoList.splice(${i},1);
+          displayList();
+        " class="todo-delete-button">Delete</button>
+      `;
+    }
+  }
+
+  document.querySelector('.js-result').innerHTML = toListHtml;
+}
